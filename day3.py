f59036abb1bc89e56b4bff77135df6a4d736c1cd
varenyamsingh <varenyamsingh1330@gmail.com>
2023-11-18 11:56:20 +0530

    This was day 4 of my python course.

---
+++ day3.py
@@ -0,0 +1,21 @@
+#In this file code for calculator is written.
+a = 50
+b = 10
+#code to find the addition between two numbers
+c = a + b
+print("sum of two numbers:", c)
+#code to find the subtraction between two numbers
+d = a - b
+print("subtracting two numbers:", d)
+#code to find the difference between two numbers
+e = a / b
+print("Difference between two numbers: ", d)
+#code to find the product of two numbers
+f = a * b
+print("Product of two numbers: ", e)
+#code to find the quotient when one number is divided by another
+g = a // b
+print("Quotient when one number is divided by another: ", f)
+#code to find the remainder when one number is divided by another
+h = a % b
+print("Remainder when one number is divided by another: ", g)
