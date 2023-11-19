18abc1da5abdd91b39e4e921c23c0e9cc892ea06
varenyamsingh <varenyamsingh1330@gmail.com>
2023-11-19 12:07:56 +0530

    These are the work done by me today.

---
+++ day4/day4.py
@@ -0,0 +1,8 @@
+# today we are doing typecasting: the conversion of one datatype to another data type.
+a = "1"
+b = "2"
+print(a + b)#this will give 12 because we have put those numbers in quotation sign soo they are recognized as string so now we will apply typecasting
+print(int(a) + int(b)) #so this will convert our string into integer
+c = 5
+d = 4
+print(c + d)
