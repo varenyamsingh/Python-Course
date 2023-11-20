3c1c1ac46a6002a19db867ba160dda3a20c3c808
varenyamsingh <varenyamsingh1330@gmail.com>
2023-11-20 20:05:34 +0530

    work done...

---
+++ day5/strings.py
@@ -0,0 +1,13 @@
+name = "Tony"
+friend = "Rony"
+print("Hello, "+ name)
+apple = '''hello today a 
+weather is good'''#triple single or double quote makes multi line strings
+print(apple)
+print(name[0])#isme hume humare outpt ke serial wise position se output milega
+print(name[3])
+print("let's use a for loop")
+for character in name:
+    print(character)
+for character in apple:
+    print(character)
