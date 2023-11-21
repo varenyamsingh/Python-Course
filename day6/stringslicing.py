f4be44a197d6fcc7fb5bfd284c7cc698f38c7854
varenyamsingh <varenyamsingh1330@gmail.com>
2023-11-21 17:39:55 +0530

    This is the work done by me today.

---
+++ day6/stringslicing.py
@@ -0,0 +1,8 @@
+names = "Tony Stark"
+print(names[0:4])#hamesha n-1 aata hai output me n=4 so 4-1 upto 0 to 3 tak aayenge
+length = (len(names))
+print("Hello i am", names, "Nice to meet you")
+print(length)
+print(names[0:-3])#last k utne words delete kar dega
+nm = "Harry"
+print(nm[-2])
